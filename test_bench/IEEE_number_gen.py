539880d3403d44954bb56a65210380b00f0547a2
Ryan Jobson <reggaeboyry@gmail.com>
2017-06-06 09:55:22 -0500

    added number generator

---
+++ test_bench/IEEE_number_gen.py
@@ -0,0 +1,26 @@
+from sys import argv
+import random
+
+
+def main():
+    """
+    This program takes two inputs, an op code and a number.
+    OP Codes
+    1 - convert number to IEEE
+    2 - Generate n random IEEE numbers and display them next to their decimal counterpart
+    3 - Generate n random and save them to a file
+    """
+    if len(argv) != 3:
+        print "Usage: op num"
+
+
+
+    op = argv[1]
+    
+
+
+
+
+
+if __name__ == "__main__":
+    main()
