20d410cdab53d23a19e325b3661717e078660784
Ryan Jobson <reggaeboyry@gmail.com>
2017-06-07 14:40:19 -0500

    fixing git issues

---
+++ test_bench/testing/IEEE_number_gen.java
@@ -0,0 +1,20 @@
+
+
+
+
+class IEEE_number_gen {
+
+  public static void main(String[] args) {
+    /*
+      This program takes two inputs, an op code and a number.
+      OP Codes
+      1 - Convert number to IEEE
+      2 - Generate n random IEEE numbers and display them next to their decimal counterpart
+      3 - Generates and converts n random numbers and save them to a file
+      */
+
+      System.out.println("5");
+  }
+
+
+}
